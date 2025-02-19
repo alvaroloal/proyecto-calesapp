52b3ab474dd734614497d88024247d0ec3727a83
alvaroloal <alorentealman@gmail.com>
2025-02-19 08:49:16 +0100

    creando proyecto

---
+++ calesapp/src/main/java/com/salesianostriana/dam/calesapp/CalesappApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.calesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalesappApplication.class, args);
+	}
+
+}
