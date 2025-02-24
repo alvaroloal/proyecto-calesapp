83d187bac9006087a61589a0135e6bc7955ef997
alvaroloal <alorentealman@gmail.com>
2025-02-24 19:43:50 +0100

    crear parada

--- calesapp/src/main/java/com/salesianostriana/dam/calesapp/dto/parada/CreateUpdateParadaDTO.java
+++ calesapp/src/main/java/com/salesianostriana/dam/calesapp/dto/parada/CreateUpdateParadaDTO.java
@@ -1,12 +1,22 @@
 package com.salesianostriana.dam.calesapp.dto.parada;
 
 import com.salesianostriana.dam.calesapp.model.Parada;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.UUID;
+
+@Schema(description = "Datos de una parada")
 public record CreateUpdateParadaDTO(
 
+        @Schema(description = "Nombre de la parada", example = "Plaza de Toros")
         String nombre,
+
+        @Schema(description = "Ubicacion de la parada", example = "40.4168,-3.7038")
         String ubicacion,
+
+        @Schema(description = "Descripción de la parada", example = "Parada en el acceso a la Plaza de toros de la Maestranza")
         String descripcion
+
 ) {
 
     public static CreateUpdateParadaDTO fromEntity(Parada parada) {
--- calesapp/src/main/java/com/salesianostriana/dam/calesapp/dto/parada/ParadaDTO.java
+++ calesapp/src/main/java/com/salesianostriana/dam/calesapp/dto/parada/ParadaDTO.java
@@ -3,6 +3,8 @@ package com.salesianostriana.dam.calesapp.dto.parada;
 import com.salesianostriana.dam.calesapp.model.Parada;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.UUID;
+
 @Schema(description = "Datos de una parada")
 public record ParadaDTO(
         @Schema(description = "ID de la parada", example = "1")
@@ -11,23 +13,24 @@ public record ParadaDTO(
         @Schema(description = "Nombre de la parada", example = "Plaza de Toros")
         String nombre,
 
-        @Schema(description = "Descripción de la parada", example = "Parada en el acceso a la Plaza de toros de la Maestranza")
-        String descripcion,
-
         @Schema(description = "Ubicacion de la parada", example = "40.4168,-3.7038")
-        String ubicacion
+        String ubicacion,
+
+        @Schema(description = "Descripción de la parada", example = "Parada en el acceso a la Plaza de toros de la Maestranza")
+        String descripcion
 ) {
 
     public static ParadaDTO fromEntity(Parada parada) {
         return new ParadaDTO(
                 parada.getId(),
                 parada.getNombre(),
-                parada.getDescripcion(),
-                parada.getUbicacion()
+                parada.getUbicacion(),
+                parada.getDescripcion()
+
         );
     }
 
     public static ParadaDTO of(Parada parada) {
-        return new ParadaDTO(parada.getId(), parada.getNombre(), parada.getDescripcion(), parada.getUbicacion());
+        return new ParadaDTO(parada.getId(), parada.getNombre(),parada.getUbicacion(), parada.getDescripcion());
     }
 }
