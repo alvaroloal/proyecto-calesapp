45c1dbbba37fbaf405d999ce5353826d17e1709a
alvaroloal <alorentealman@gmail.com>
2025-06-12 09:56:38 +0200

    manejo de errores orden

--- calesapp/src/main/java/com/salesianostriana/dam/calesapp/error/GlobalErrorController.java
+++ calesapp/src/main/java/com/salesianostriana/dam/calesapp/error/GlobalErrorController.java
@@ -3,7 +3,8 @@ package com.salesianostriana.dam.calesapp.error;
 import jakarta.persistence.EntityNotFoundException;
 import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -12,26 +13,23 @@ import java.net.URI;
 
 @RestControllerAdvice
 @Order(2)
-public class GlobalErrorController
-        extends ResponseEntityExceptionHandler {
+public class GlobalErrorController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ProblemDetail handleEntityNotFound(EntityNotFoundException ex) {
-        ProblemDetail result = ProblemDetail
-                .forStatusAndDetail(HttpStatus.NOT_FOUND,
-                        ex.getMessage());
-        result.setTitle("Entidad no encontrada");
-        result.setType(URI.create("https://www.salesianos-triana.edu/errors/entity-not-found"));
-        return result;
+        ProblemDetail detalle = ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND, ex.getMessage());
+        detalle.setTitle("Entidad no encontrada");
+        detalle.setType(URI.create("https://www.salesianos-triana.edu/errors/entity-not-found"));
+        return detalle;
     }
 
     @ExceptionHandler(AuthenticationException.class)
     public ProblemDetail handleAuthenticationError(AuthenticationException ex) {
-        ProblemDetail result = ProblemDetail
-                .forStatusAndDetail(HttpStatus.UNAUTHORIZED,
-                        ex.getMessage());
-        result.setTitle("Operación no permitida");
-        return result;
+        ProblemDetail detalle = ProblemDetail.forStatusAndDetail(
+                HttpStatus.UNAUTHORIZED, ex.getMessage());
+        detalle.setTitle("Operación no permitida");
+        detalle.setType(URI.create("https://www.salesianos-triana.edu/errors/authentication-failed"));
+        return detalle;
     }
-
 }
