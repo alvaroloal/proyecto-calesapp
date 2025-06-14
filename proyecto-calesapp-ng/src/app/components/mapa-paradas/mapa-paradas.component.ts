1b0b199daab179b02ea234c40765e7a3afeda1be
alvaroloal <alorentealman@gmail.com>
2025-06-14 20:15:32 +0200

    ver detalles de paradas mapa

--- proyecto-calesapp-ng/src/app/components/mapa-paradas/mapa-paradas.component.ts
+++ proyecto-calesapp-ng/src/app/components/mapa-paradas/mapa-paradas.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { GoogleMapsModule, MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { ParadasService } from '../../services/paradas/paradas.service';
 import { Parada } from '../../models/parada.model';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-mapa-paradas',
@@ -12,19 +15,45 @@ import { Parada } from '../../models/parada.model';
 })
 export class MapaParadasComponent implements OnInit {
   paradas: Parada[] = [];
+  paradaSeleccionada: Parada | null = null;
 
-  center: google.maps.LatLngLiteral = { lat: 37.3861, lng: -5.9922 }; // Centro Sevilla
+
+  center: google.maps.LatLngLiteral = { lat: 37.3861, lng: -5.9922 };
   zoom = 14;
   markerOptions: google.maps.MarkerOptions = { animation: google.maps.Animation.DROP };
 
-  constructor(private paradasService: ParadasService) { }
+  @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
+
+  constructor(private paradasService: ParadasService, private router: Router) { }
 
   ngOnInit(): void {
     this.paradasService.getParadas().subscribe({
       next: (data) => {
-        this.paradas = data.filter(p => p.lat && p.lng); // Solo las que tengan coordenadas
+        this.paradas = data.filter(p => p.lat && p.lng);
       },
       error: (err) => console.error('Error cargando paradas para el mapa', err)
     });
   }
+
+  abrirInfoWindow(parada: Parada, marker: MapMarker): void {
+    this.paradaSeleccionada = parada;
+    this.infoWindow.open(marker);
+  }
+
+
+  verDetallesParada(): void {
+    if (this.paradaSeleccionada?.id) {
+      this.infoWindow.close();
+      this.router.navigate(['/paradas', this.paradaSeleccionada.id]).then(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      });
+    }
+  }
+
+  cerrarInfoWindow(): void {
+    this.infoWindow.close();
+    this.paradaSeleccionada = null;
+  }
+
+
 }
